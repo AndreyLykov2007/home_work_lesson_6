86649762e6d7591f22b707dd4784577f74e3d3ca
Andrey Lykov <lykov2007@gmail.com>
2024-04-11 14:18:49 +0100

    init

---
+++ src/test/java/MathAndLogicOperatorsPractice.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+public class MathAndLogicOperatorsPractice {
+    //Матемачические операторы
+    // Сложение, вычитание, деление, умножение, остаток от деления, инкремент, декремент
+    int Aint = 10;
+    int Bint = 20;
+    int Result;
+
+    //Матемачические Integer
+    @Test
+    void MathOperatorsPractice() {
+    System.out.println(Bint + Aint);
+    System.out.println(Bint - Aint);
+    System.out.println(Bint * Aint);
+    System.out.println(Bint / Aint);
+    System.out.println(Aint % Bint);
+    System.out.println(++Bint);
+    System.out.println(--Aint);
+    }
+
+// Допится перевыполнения по каждому примитивному типу данных+String: byte, short, int, long, float, double, String
+// Логические: &&, &, |, ||, !
+
+
+    //Попробовать вычисления комбинации типов данных: double+int, double+float, byte+long, sort+long и т.д.
+}
