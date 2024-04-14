e6ce68e6a7c026da704c956eb774ea07c48050d0
Andrey Lykov <lykov2007@gmail.com>
2024-04-14 13:54:26 +0100

    removed code to src/main

--- src/main/java/MathAndLogicOperatorsPractice.java
+++ src/main/java/MathAndLogicOperatorsPractice.java
@@ -1,16 +1,14 @@
-import org.junit.jupiter.api.Test;
-
 public class MathAndLogicOperatorsPractice {
-    int Aint = 10;
-    int Bint = 20;
-    boolean Abln = true;
-    boolean Bbln = false;
-    double Cdbl = 13.54321;
+
+   public static void main(String[] args) {
+        int Aint = 10;
+        int Bint = 20;
+        boolean Abln = true;
+        boolean Bbln = false;
+        double Cdbl = 13.54321;
 
 
-    @Test
-    void MathOperatorsPractice() {
-        System.out.println("\nМатематические операторы. b=20 and a=10");
+        System.out.println("\nМатематические операторы. a=10 and b=20");
         System.out.println("b - a = " + (Bint - Aint));
         System.out.println("b * a = " + (Bint * Aint));
         System.out.println("b / a = " + (Bint / Aint));
@@ -18,7 +16,7 @@ public class MathAndLogicOperatorsPractice {
         System.out.println("b++ = " + (++Bint));
         System.out.println("b-- = " + (--Bint));
 
-        System.out.println("\nПереполнение по типам данных");
+        System.out.println("\nОператоры сравнения");
         System.out.println("a = b - " + (Aint == Bint));
         System.out.println("a не равно b - " + (Aint != Bint));
         System.out.println("a > b - " + (Aint > Bint));
@@ -33,8 +31,8 @@ public class MathAndLogicOperatorsPractice {
         System.out.println("Перепеоление short до -32769 результат = " + ((short) (-32769)));
         System.out.println("Перепеоление long до 9223372036854775808 результат = " + ((long) (9223372036854775807L + 1)));
         System.out.println("Перепеоление long до -9223372036854775809 результат = " + ((long) (-9223372036854775808L - 1)));
-        System.out.println("Перепеоление float результат = " + ((float) (3.4028235 * Math.pow(10, 38) * 2)));
-        System.out.println("Перепеоление double результат = " + ((double) (-1.7976931348623157 * Math.pow(10, 308) * 2)));
+        System.out.println("Перепеоление положительного максимума float результат = " + ((float) (3.4028235 * Math.pow(10, 38) * 2)));
+        System.out.println("Перепеоление отрицательного максимума double результат = " + ((double) (-1.7976931348623157 * Math.pow(10, 308) * 2)));
 
         System.out.println("\nЛогические операторы. a=true and b=false");
         System.out.println("a И b истина или ложь - " + (Abln && Bbln));
